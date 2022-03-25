fae8e1ebb24348543391fd537751440f8ce11b98
Nicola Casarin <nico.casarin@icloud.com>
2022-03-25 21:25:44 +0100

    Create cv.R

---
+++ cv.R
@@ -0,0 +1,7 @@
+library(datadrivencv)
+
+use_datadriven_cv(
+	full_name = "Nicola Casarin",
+	data_location = "https://docs.google.com/spreadsheets/d/10vwmcLHM2Lpy0VE9xUhyq1exUQ25vwrPzNufKSO8s3w/edit?usp=sharing"
+	
+)
