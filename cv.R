65f6442b778ffbe159c03fc1a94847fc5e4389ee
Nicola Casarin <62801213+n-oise@users.noreply.github.com>
2022-04-10 19:42:31 +0200

    Delete cv.R

--- cv.R
+++
@@ -1,9 +0,0 @@
-library(datadrivencv)
-
-use_datadriven_cv(
-	full_name = "Nicola Casarin",
-	data_location = "https://docs.google.com/spreadsheets/d/10vwmcLHM2Lpy0VE9xUhyq1exUQ25vwrPzNufKSO8s3w/edit?usp=sharing",
-	html_location = "ncasa.it/cv",
-	source_location = "",
-	pdf_location = "https://github.com/n-oise/",
-)
